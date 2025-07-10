ee421e3e706f67f3ee00b01ae2e28c67a5bce0b0
Syafiq Aiman <isyafiq@gmail.com>
2025-07-10 17:23:04 +0800

    feat: auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -14,7 +24,17 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @Post('register')
   register(@Body() signInDto: Record<string, any>) {
-    console.log(signInDto.email, signInDto.password, signInDto.name)
-    return this.authService.register(signInDto.email, signInDto.password, signInDto.name);
+    console.log(signInDto.email, signInDto.password, signInDto.name);
+    return this.authService.register(
+      signInDto.email,
+      signInDto.password,
+      signInDto.name,
+    );
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) {
+    return req.user;
   }
 }
