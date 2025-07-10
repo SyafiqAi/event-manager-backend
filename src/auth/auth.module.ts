dbf76a8d8183857095ce9dc9fca2b4af79178af1
Syafiq Aiman <isyafiq@gmail.com>
2025-07-10 17:59:15 +0800

    refactor: folder structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
+import { jwtConstants } from './constants/jwtConstants';
 
 @Module({
   imports: [
