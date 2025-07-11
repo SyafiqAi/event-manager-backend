84e1c505bac5363249512c820a4536294e3aba89
Syafiq Aiman <isyafiq@gmail.com>
2025-07-11 15:50:08 +0800

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors()
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
@@ -25,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(9000);
 }
 bootstrap();
