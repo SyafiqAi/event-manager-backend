76d57f318e5ec4454a9e78e7ab9857f6e3c459d0
Syafiq Aiman <isyafiq@gmail.com>
2025-07-11 10:56:10 +0800

    chore: cleanup

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,11 +1,11 @@
-import { Body, Injectable, Param } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEventDto } from './dto/create-event.dto';
 
 @Injectable()
 export class EventService {
   constructor(private readonly prismaService: PrismaService) {}
-  create(@Body() dto: CreateEventDto) {
+  create(dto: CreateEventDto) {
     return this.prismaService.event.create({ data: dto });
   }
 
@@ -13,15 +13,15 @@ export class EventService {
     return this.prismaService.event.findMany();
   }
 
-  findOne(@Param('id') id: number) {
+  findOne(id: number) {
     return this.prismaService.event.findUnique({ where: { id } });
   }
 
-  update(@Param('id') id: number, @Body() dto: Partial<CreateEventDto>) {
+  update(id: number,dto: Partial<CreateEventDto>) {
     return this.prismaService.event.update({ where: { id }, data: dto });
   }
 
-  remove(@Param('id') id: number) {
+  remove(id: number) {
     return this.prismaService.event.delete({ where: { id } });
   }
 }
